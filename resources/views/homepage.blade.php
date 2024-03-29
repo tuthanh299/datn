c53cf35397a92b4123ef649f58dc9bdb98344225
tuthanh299 <113401675+tuthanh299@users.noreply.github.com>
2024-03-29 16:35:57 +0700

    Update homepage.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -20,6 +20,17 @@
             rel="stylesheet"
             integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
             crossorigin="anonymous">
+        <script type="text/javascript">
+        function GoogleLanguageTranslatorInit() {
+            new google.translate.TranslateElement({
+                pageLanguage: 'vi',
+                autoDisplay: false
+            }, 'google_language_translator');
+        }
+    </script>
+    <script type="text/javascript" src="http://translate.google.com/translate_a/element.js?cb=GoogleLanguageTranslatorInit" async defer></script>
+    <script type='text/javascript' src='flags.js'></script>
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
         <script
             src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script type="text/javascript"
